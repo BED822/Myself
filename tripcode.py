d75abced53280cd09179f7b24e686d1d7439e36d
Donald Tsang <brandonknlee@gmail.com>
2015-12-04 10:45:54 +0800

    Update tripcode.py

--- tripcode.py
+++ tripcode.py
@@ -2,11 +2,11 @@ def mktripcode(password, length):
 	if l < 8:
 		assert error "length is less than 8"
 	password = password.decode('utf_8', 'ignore') \
-		.encode('shift_jis', 'ignore')    \
-		.replace('"', '&quot;')      \
-		.replace("'", '')           \
-		.replace('<', '&lt;')        \
-		.replace('>', '&gt;')        \
+		.encode('shift_jis', 'ignore') \
+		.replace('"', '&quot;') \
+		.replace("'", '') \
+		.replace('<', '&lt;') \
+		.replace('>', '&gt;') \
 		.replace(',', ',')
 	salt = (password + '...')[1:3]
 	salt = re.compile('[^\.-z]').sub('.', salt)
