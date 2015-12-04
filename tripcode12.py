928d6f170f18b70dc4a4e67bc71e9d63d5aa5a16
Donald Tsang <brandonknlee@gmail.com>
2015-12-04 11:12:13 +0800

    Update tripcode12.py

--- tripcode12.py
+++ tripcode12.py
@@ -1,11 +1,12 @@
 import base64
 import hashlib
 
-def trip12(key):
+def trip12(key, length):
+	assert 27 > len(key) >= 8
 	key = key.encode('cp932') 
-	assert len(key) >= 12
+	assert len(key) >= length
 	code = hashlib.sha1(key).digest()
 	code = base64.b64encode(code, b'./')
-	code = code[:12]
+	code = code[:length]
 	code = code.decode('cp932')
 	return code
