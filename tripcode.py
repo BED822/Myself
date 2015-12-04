7564f2a5ebd23b06c3426af7bd36c84846415795
Donald Tsang <brandonknlee@gmail.com>
2015-12-04 10:42:35 +0800

    Update tripcode.py

--- tripcode.py
+++ tripcode.py
@@ -1,13 +1,15 @@
-def mktripcode(pw):
-	pw = pw.decode('utf_8', 'ignore') \
+def mktripcode(password, length):
+	if l < 8:
+		assert error "length is less than 8"
+	password = password.decode('utf_8', 'ignore') \
 		.encode('shift_jis', 'ignore')    \
 		.replace('"', '&quot;')      \
 		.replace("'", '')           \
 		.replace('<', '&lt;')        \
 		.replace('>', '&gt;')        \
 		.replace(',', ',')
-	salt = (pw + '...')[1:3]
+	salt = (password + '...')[1:3]
 	salt = re.compile('[^\.-z]').sub('.', salt)
 	salt = salt.translate(string.maketrans(':;<=>?@[\\]^_`', 'ABCDEFGabcdef'))
-	trip = crypt.crypt(pw, salt)[-10:]
+	trip = crypt.crypt(password, salt)[-length:]
 	return trip
