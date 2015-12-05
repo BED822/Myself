07f18bc34287f257eda311346a832ef2a78f8ac1
Donald Tsang <brandonknlee@gmail.com>
2015-12-06 01:11:17 +0800

    Update remBin.py

--- remBin.py
+++ remBin.py
@@ -1,7 +1,25 @@
 def remBin(a, n):
 	assert a >= 0, "a is not positive"
+	x = ( 1 << n ) - 1
 	while a > x:
 		r = a % x
 		a = a >> n
 		a += r
 	return a
+"""
+unsigned int n;                      // numerator
+const unsigned int s;                // s > 0
+const unsigned int d = (1 << s) - 1; // so d is either 1, 3, 7, 15, 31, ...).
+unsigned int m;                      // n % d goes here.
+
+for (m = n; n > d; n = m)
+{
+  for (m = 0; n; n >>= s)
+  {
+    m += n & d;
+  }
+}
+// Now m is a value from 0 to d, but since with modulus division
+// we want m to be 0 when it is d.
+m = m == d ? 0 : m;
+"""
