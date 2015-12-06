1aadcad2bf97010d5908378af86b3fca36645b91
Donald Tsang <brandonknlee@gmail.com>
2015-12-06 11:31:10 +0800

    Update int2base.py

--- int2base.py
+++ int2base.py
@@ -157,3 +157,12 @@ def egcd(big, small):
 		old_y = new_y
 		new_y = sum
 	return new_y, old_y
+
+def egcd_fast(a, b):
+	x,y, u,v = 0,1, 1,0
+	while a != 0:
+		q, r = b//a, b%a
+		m, n = x-u*q, y-v*q
+	        b,a, x,y, u,v = a,r, u,v, m,n
+	gcd = b
+	return gcd, x, y
