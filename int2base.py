53dbfc6a9e4d49b551a19dc41686f14efbbdcc05
Donald Tsang <brandonknlee@gmail.com>
2015-12-06 11:32:45 +0800

    Update int2base.py

--- int2base.py
+++ int2base.py
@@ -166,3 +166,17 @@ def egcd_fast(a, b):
 	        b,a, x,y, u,v = a,r, u,v, m,n
 	gcd = b
 	return gcd, x, y
+
+def egcd(a, b):
+	if a == 0:
+		return (b, 0, 1)
+	else:
+		g, y, x = egcd(b % a, a)
+	return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+	gcd, x, y = egcd(a, m)
+	if gcd != 1:
+		return None  # modular inverse does not exist
+	else:
+		return x % m
