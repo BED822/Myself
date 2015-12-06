bea8052e524436104eadf4e2920c367dfec61b49
Donald Tsang <brandonknlee@gmail.com>
2015-12-06 10:29:47 +0800

    Update base62.py

--- base62.py
+++ base62.py
@@ -1,43 +1,43 @@
 digits='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def decode256(str):
-	assert len(str) <= 43, "too long"
+def encode(num, exp):
+	if exp == 256:
+		base, limit = 62, 43
+	elif exp == 160:
+		base, limit = 61, 27
+		base = 61
+		limit = 27
+	elif exp == 112:
+		base, limit = 60, 19
+	else:
+		assert "exp not valid"
+	assert num >= 0, "not positive"
+	assert num < (2 ** exp), "too big"
+	if num == 0:
+		return '0'.zfill(limit)
+	str=''
+	while num != 0:
+		num, char = divmod(num, base)
+		str = (digits[char]) + str
+	return str.zfill(limit)
+
+def decode(num, exp):
+	if exp == 256:
+		base = 62
+		limit = 43
+	elif exp == 160:
+		base = 61
+		limit = 27
+	elif exp == 112:
+		base = 60
+		limit = 19
+	else:
+		assert "exp not valid"
+	assert len(str) <= limit, "too long"
 	str = str.lstrip("0")
 	num , mult = 0 , 1
 	for char in reversed(str):
 		num += mult * digits.index(char)
-		mult *= 62
-	assert num < (2 ** 256), "number error"
+		mult *= base
+	assert num < (2 ** exp), "number error"
 	return num
-
-def encode256(num):
-	assert num >= 0, "not positive"
-	assert num < (2 ** 256), "too big"
-	if num == 0:
-		return '0'.zfill(43)
-	str=''
-	while num != 0:
-		num, char = divmod(num, 62)
-		str = (digits[char]) + str
-	return str.zfill(43)
-
-def decode160(str):
-	assert len(str) <= 27, "too long"
-	str = str.lstrip("0")
-	num , mult = 0 , 1
-	for char in reversed(str):
-		num += mult * digits.index(char)
-		mult *= 61
-	assert num < (2 ** 160), "number error"
-	return num
-
-def encode160(num):
-	assert num >= 0, "not positive"
-	assert num < (2 ** 160), "too big"
-	if num == 0:
-		return '0'.zfill(43)
-	str=''
-	while num != 0:
-		num, char = divmod(num, 61)
-		str = (digits[char]) + str
-	return str.zfill(27)
