03a8f3c10b982ea23413ab63485e6b45a07c101c
Donald Tsang <brandonknlee@gmail.com>
2015-12-07 22:13:03 +0800

    Update base62.py

--- base62.py
+++ base62.py
@@ -1,14 +1,13 @@
 digits='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def encode(num, exp):
+	assert exp == 256 or exp == 160 or exp == 112, "exp not valid"
 	if exp == 256:
 		base, limit = 62, 43
 	elif exp == 160:
 		base, limit = 61, 27
 	elif exp == 112:
 		base, limit = 60, 19
-	else:
-		assert "exp not valid"
 	assert num >= 0, "not positive"
 	assert num < (2 ** exp), "too big"
 	if num == 0:
@@ -20,14 +19,13 @@ def encode(num, exp):
 	return str.zfill(limit)
 
 def decode(num, exp):
+	assert exp == 256 or exp == 160 or exp == 112, "exp not valid"
 	if exp == 256:
 		base, limit = 62, 43
 	elif exp == 160:
 		base, limit = 61, 27
 	elif exp == 112:
 		base, limit = 60, 19
-	else:
-		assert "exp not valid"
 	assert len(str) <= limit, "too long"
 	str = str.lstrip("0")
 	num , mult = 0 , 1
