4728dafaf8aea7d188ab0d065bbfd1eec860a68d
Donald Tsang <brandonknlee@gmail.com>
2015-12-10 10:36:34 +0800

    Update base62.py

--- base62.py
+++ base62.py
@@ -34,36 +34,3 @@ def decode(num, exp):
 		mult *= base
 	assert num < (2 ** exp), "number error"
 	return num
-
-def rem31(n):
-	m = n
-	while n > 31:
-		m = 0
-		while n != 0:
-			m += n & 31
-			n >>= 5
-		n = m
-	if m == 31:
-		return 0
-	return m
-
-def rem15(n):
-	m = n
-	while n > 15:
-		m = 0
-		while n != 0:
-			m += n & 15
-			n >>= 4
-		n = m
-	if m == 15:
-		return 0
-	return m
-
-"""
-if x & 1 = a and x % 31 = b
-then x % 62 = a * 31 * (((31)^(-1))%2) + b * 2 * (((2)^(-1))%31)
-or x % 62 = 31 * a + 32 * b (not final form)
-if x & 3 = a and x % 31 = b
-then x % 60 = a * 15 * (((15)^(-1))%4) + b * 4 * (((4)^(-1))%15)
-or x % 60 = 45 * a + 16 * b (not final form)
-"""
