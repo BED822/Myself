96835bd65d51c938639024903e923f05575576f2
Donald Tsang <brandonknlee@gmail.com>
2015-12-23 17:42:39 +0800

    Update RemCurve.py

--- RemCurve.py
+++ RemCurve.py
@@ -23,3 +23,21 @@ rem521(n):
 	if m + 1 >= (1 << 521):
 		return m + 1 - (1 << 521)
 	return m
+
+"""
+unsigned int n;                      // numerator
+const unsigned int s;                // s > 0
+const unsigned int d = (1 << s) - 1; // so d is either 1, 3, 7, 15, 31, ...).
+unsigned int m;                      // n % d goes here.
+
+for (m = n; n > d; n = m)
+{
+  for (m = 0; n; n >>= s)
+  {
+    m += n & d;
+  }
+}
+// Now m is a value from 0 to d, but since with modulus division
+// we want m to be 0 when it is d.
+m = m == d ? 0 : m;
+"""
