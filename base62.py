584ffb5a341e08090d14f1df41639184e246621c
Donald Tsang <brandonknlee@gmail.com>
2016-06-07 17:30:46 +0800

    Update base62.py

--- base62.py
+++ base62.py
@@ -1,6 +1,8 @@
 # [0, 3, 6, 9, 11, 14, 17, 19, 22, 25, 27, 30, 33, 35, 38, 41, 43]
 # n-th element = number of character needed to represent 16*i bits
 
+digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
 def number_of_characters(bits, base):
 	assert bits % 16 == 0, "not 16"
 	bits //= 16
